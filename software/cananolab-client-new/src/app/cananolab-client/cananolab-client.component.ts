dbb233e0219bc4a2155a941d0887dde912218f83
lernermh <martin.lerner@nih.gov>
2022-01-18 06:00:56 -0500

    Initial commit

--- software/cananolab-client-new/src/app/cananolab-client/cananolab-client.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/cananolab-client.component.ts
@@ -9,20 +9,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigurationService } from './common/services/configuration.service';
 import { Properties } from '../../assets/properties';
+import { Router } from '@angular/router';
+import { UtilService } from './common/services/util.service';
 
-@Component({
-  selector: 'canano-cananolab-client',
-  templateUrl: './cananolab-client.component.html',
-  styleUrls: ['./cananolab-client.component.scss']
-})
-export class CananolabClientComponent implements OnInit {
+@Component( {
+    selector: 'canano-cananolab-client',
+    templateUrl: './cananolab-client.component.html',
+    styleUrls: ['./cananolab-client.component.scss']
+} )
+export class CananolabClientComponent implements OnInit{
 
     properties = Properties;
 
-  constructor(private configurationService: ConfigurationService) {
-  }
+    constructor( private configurationService: ConfigurationService, private router: Router,
+                 private utilService: UtilService ){
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void{
+      //  this.init();
+    }
 
+    async init(){
+        // @FIXME - @TODO change this to a "We are initialized" flag
+        //  await this.utilService.sleep( 5000 );
+        //  console.log('MHL Set route to /home');
+        // this.router.navigate(['home']);
+    }
 }
