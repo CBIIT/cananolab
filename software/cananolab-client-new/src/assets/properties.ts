69d0209fa6341f55c38b24cb15220ee6c9cc372a
lernermh <martin.lerner@nih.gov>
2022-01-18 05:55:05 -0500

    Initial commit

--- software/cananolab-client-new/src/assets/properties.ts
+++ software/cananolab-client-new/src/assets/properties.ts
@@ -1,8 +1,14 @@
 export let Properties = {
 
     // Be sure API_SERVER_URL is an empty string for non development environments (no ng start)
-    // API_SERVER_URL: '',
-     API_SERVER_URL: 'http://192.168.1.25:4600/caNanoLab',
+    API_SERVER_URL: '',
+    // API_SERVER_URL: 'http://192.168.1.25:4600',
+
+    MAX_PAGER_BUTTONS: 6,
+    DEFAULT_PAGE_LENGTH: 10,
+    MAX_PAGE_LENGTH: 200,
+
+    DISCLAIMER_URL: 'html/cananoDisclaimer.html',
 
     // Show in console, curl version of post & get calls.
     DEBUG_CURL: true,
@@ -10,10 +16,22 @@ export let Properties = {
     // This value can be changed in the config file.
     HTTP_TIMEOUT: 120000, // Thousands of a second
 
+    MAX_WIDTH: '1600px',  // @TODO make this a configuration file thing
+
     CONFIG_FILE: 'configuration',
     CONFIG_COMPLETE: false, // @TODO Do not forget to implement checking for & updating this value
 
     // Don't access the server, only use test data.
-    TEST_MODE: false
+    TEST_MODE: false,
+
+    current_user: 'xx',
+    logged_in: false,
+    LOGGED_IN: false,
+
+    COUNTER: 0,
+
+    PROTOCOL_TYPES: [],
+    PROTOCOL_CSM_ROLES: []
+
 
 }
