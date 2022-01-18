058f6e2b6992451e8656fcb788f841c9939d1872
lernermh <martin.lerner@nih.gov>
2022-01-18 05:58:43 -0500

    Initial commit

--- software/cananolab-client-new/src/app/cananolab-client/header/top-keyword-search/top-keyword-search.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/header/top-keyword-search/top-keyword-search.component.ts
@@ -2,47 +2,64 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../common/services/api.service';
 import { Consts } from '../../../constants';
 
-@Component({
-  selector: 'canano-top-keyword-search',
-  templateUrl: './top-keyword-search.component.html',
-  styleUrls: ['./top-keyword-search.component.scss']
-})
-export class TopKeywordSearchComponent implements OnInit {
+@Component( {
+    selector: 'canano-top-keyword-search',
+    templateUrl: './top-keyword-search.component.html',
+    styleUrls: ['./top-keyword-search.component.scss']
+} )
+export class TopKeywordSearchComponent implements OnInit{
     topKeyWordSearchText = '';
-  constructor( private apiService: ApiService) { }
 
-  ngOnInit(): void {
-  }
-
-    onTopKeyWordSearchClick(){
-      console.log('MHL onTopKeyWordSearchClick topKeyWordSearchText: ', this.topKeyWordSearchText );
-      // /caNanoLab/rest/customsearch/search?keyword={{topKeyWordSearchText}}
-
-
-      //  this.apiService.doPost( Consts.QUERY_SEARCH, 'keyword=' + this.topKeyWordSearchText ).subscribe(
-        this.apiService.doGet( Consts.QUERY_SEARCH, 'keyword=' + this.topKeyWordSearchText ).subscribe(
-            data => {
-            console.log('MHL onTopKeyWordSearchClick: ', data);
-                // @TESTING
-                this.getUserGroups()
-            },
-            ( err ) => {
-                console.log('MHL ERROR onTopKeyWordSearchClick: ', err);
-            });
+    constructor( private apiService: ApiService ){
     }
 
-    // @TESTING
-    getUserGroups(){ // security/getUserGroups
-        console.log('MHL getUserGroups');
+    ngOnInit(): void{
+    }
+
+    onTopKeyWordSearchClick(){
+        console.log( 'MHL onTopKeyWordSearchClick topKeyWordSearchText: ', this.topKeyWordSearchText );
+
+
+        //  @TODO
+        this.apiService.doGet( Consts.QUERY_SEARCH, 'keyword=' + this.topKeyWordSearchText ).subscribe(
+            data => {
+                console.log( 'MHL onTopKeyWordSearchClick: ', data );
+
+            },
+            ( err ) => {
+                console.log( 'MHL ERROR onTopKeyWordSearchClick: ', err );
+            } );
+    }
+
+    // @TODO this will soon be duplicate code
+    getUserGroups(){
 
         //  this.apiService.doPost( Consts.QUERY_SEARCH, 'keyword=' + this.topKeyWordSearchText ).subscribe(
         this.apiService.doGet( Consts.QUERY_GET_USER_GROUPS, '' ).subscribe(
             data => {
-                console.log('MHL getUserGroups: ', data);
+                console.log( 'MHL getUserGroups: ', data );
             },
             ( err ) => {
-                console.log('MHL ERROR getUserGroups: ', err);
-            });
+                console.log( 'MHL ERROR getUserGroups: ', err );
+            } );
+
+    }
+
+    /**
+     *
+     */
+    getUsers(){
+        // security/gets
+        console.log( 'MHL getUsers' );
+
+        //  this.apiService.doPost( Consts.QUERY_SEARCH, 'keyword=' + this.topKeyWordSearchText ).subscribe(
+        this.apiService.doGet( Consts.QUERY_GET_USERS, '*' ).subscribe(
+            data => {
+                console.log( 'MHL getUsers data: ', data );
+            },
+            ( err ) => {
+                console.log( 'MHL ERROR getUsers err: ', err );
+            } );
 
     }
 }
