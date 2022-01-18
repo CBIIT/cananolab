51748d7a156f0decb550f2e1cd28c2933703440b
lernermh <martin.lerner@nih.gov>
2022-01-18 05:08:04 -0500

    Initial commit of new Angular client.(cont)

---
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/advanced-sample/advanced-sample.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'canano-advanced-sample',
+  templateUrl: './advanced-sample.component.html',
+  styleUrls: ['./advanced-sample.component.scss']
+})
+export class AdvancedSampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
