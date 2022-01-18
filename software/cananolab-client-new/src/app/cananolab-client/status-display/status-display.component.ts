a27bd8a116ad741001b5f40201e4f1ed2cfc60c6
lernermh <martin.lerner@nih.gov>
2022-01-18 05:53:18 -0500

    Initial commit

---
+++ software/cananolab-client-new/src/app/cananolab-client/status-display/status-display.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { StatusDisplayService } from './status-display.service';
+import { timeout } from 'rxjs/operators';
+import { Properties } from '../../../assets/properties';
+
+@Component({
+  selector: 'canano-status-display',
+  templateUrl: './status-display.component.html',
+  styleUrls: ['./status-display.component.scss']
+})
+export class StatusDisplayComponent implements OnInit {
+
+    userName = 'TEST User';
+  constructor( private statusDisplayService: StatusDisplayService) { }
+
+  ngOnInit(): void {
+      this.statusDisplayService.updateUserEmitter.pipe( timeout( Properties.HTTP_TIMEOUT ) ).subscribe(
+          data => {
+              this.userName = data;
+          });
+
+  }
+
+}
---
+++ software/cananolab-client-new/src/app/cananolab-client/status-display/status-display.service.ts
@@ -0,0 +1,42 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Consts } from '../../constants';
+import { ApiService } from '../common/services/api.service';
+import { Properties } from '../../../assets/properties';
+
+@Injectable( {
+    providedIn: 'root'
+} )
+export class StatusDisplayService{
+
+    updateUserEmitter = new EventEmitter();
+    user = '';
+
+    constructor( private apiService: ApiService ){
+    }
+
+    getUser(){
+        return this.user;
+    }
+
+    updateUser( user ){
+        this.user = user;
+        this.updateUserEmitter.emit( user );
+        this.updateUserGroups();
+    }
+
+    updateUserGroups(){
+
+        //  this.apiService.doPost( Consts.QUERY_SEARCH, 'keyword=' + this.topKeyWordSearchText ).subscribe(
+        this.apiService.doGet( Consts.QUERY_GET_USER_GROUPS, '' ).subscribe(
+            data => {
+                // Set user as "Logged in"
+                Properties.logged_in = true;
+                console.log( 'MHL getUserGroups: ', data );
+            },
+            ( err ) => {
+                console.log( 'MHL ERROR getUserGroups: ', err );
+            } );
+
+    }
+
+}
