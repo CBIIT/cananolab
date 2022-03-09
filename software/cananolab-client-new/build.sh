518fae81f20fe7c38a366a2a6688311699fa194e
regulator95 <einolfs@nih.gov>
2022-03-09 15:26:55 -0500

    test

--- software/cananolab-client-new/build.sh
+++ software/cananolab-client-new/build.sh
@@ -4,4 +4,5 @@ ng build --base-href '.' --output-path /tmp/cananolab_new_client
 cd /tmp/cananolab_new_client
 jar -cvf cananolab_new_client.war *
 
+
 cp cananolab_new_client.war /opt/wildfly-8.2.1.Final/standalone/deployments
