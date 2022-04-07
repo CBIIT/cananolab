56bee27e4f0ccb4419cf8405a1c9f909e15036ff
Your Name <you@example.com>
2022-04-07 10:22:28 -0400

    checking in chemical association

--- software/cananolab-client-new/src/app/cananolab-client/main-display/samples/composition/chemicalassociation/chemicalassociation.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/samples/composition/chemicalassociation/chemicalassociation.component.ts
@@ -12,17 +12,106 @@ import { Router } from '@angular/router';
   styleUrls: ['./chemicalassociation.component.scss']
 })
 export class ChemicalassociationComponent implements OnInit {
+  sampleId = Properties.CURRENT_SAMPLE_ID;
+  dataId;
+  sampleName = Properties.CURRENT_SAMPLE_NAME;
+  helpUrl =  Consts.HELP_URL_SAMPLE_COMPOSITION;
+  toolHeadingNameManage = 'Sample Composition';
+  chemicalAssociationData;
+  setupData;
 
     constructor( private router: Router, private route: ActivatedRoute,private httpClient: HttpClient ){
     }
 
-  ngOnInit(): void{
-    this.route.params.subscribe(
-        ( params: Params ) => {
-          console.log(params['sampleId'])
-          console.log(params['dataId'])
-        } 
-    );
-}
+
+    ngOnInit(): void{
+      this.route.params.subscribe(
+          ( params: Params ) => {
+            this.sampleId = params['sampleId'];
+            this.dataId = params['dataId'];
+            if(
+                  this.sampleId <= 0 ){
+                  this.sampleId = Properties.CURRENT_SAMPLE_ID;
+              }else{
+                  Properties.CURRENT_SAMPLE_ID = this.sampleId;
+              };
+              this.chemicalAssociationData = this.getChemicalAssociationData().subscribe(
+                data => {
+                    console.log(data)
+                    Properties.SAMPLE_TOOLS = true;
+                    this.chemicalAssociationData = data;
+                    Properties.CURRENT_SAMPLE_NAME = data['sampleName'];
+                } );  
+                this.setupData = this.getSetupData().subscribe(
+                    data => {
+                        Properties.SAMPLE_TOOLS = true;
+                        this.setupData = data;
+                    } );                     
+                // this.dropdowns = this.getSetupData().subscribe(
+                //   data => {
+                //       console.log(data)
+                //       Properties.SAMPLE_TOOLS = true;
+                //       this.compositionData = data;
+                //       Properties.CURRENT_SAMPLE_NAME = data['sampleName'];
+                //   } );                                
+          } 
+      );
+  }
+
+  getChemicalAssociationData(){
+    let getUrl = Properties.API_SERVER_URL + '/caNanoLab/rest/chemicalAssociation/edit?sampleId=' + this.sampleId + '&dataId=' + this.dataId;
+
+    if( Properties.DEBUG_CURL ){
+        let curl = 'curl  -k \'' + getUrl + '\'';
+        console.log( curl );
+    }
+
+    let headers = new HttpHeaders( {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    } );
+
+    let options = {
+        headers: headers,
+        method: 'get',
+    };
+
+    let results;
+    try{
+        results = this.httpClient.get( getUrl, options ).pipe( timeout( Properties.HTTP_TIMEOUT ) );
+    }catch( e ){
+        // TODO react to error.
+        console.error( 'doGet Exception: ' + e );
+    }
+    return results;
+
+} 
+
+getSetupData(){
+    let getUrl = Properties.API_SERVER_URL + '/caNanoLab/rest/chemicalAssociation/setup?sampleId=' + this.sampleId;
+
+    if( Properties.DEBUG_CURL ){
+        let curl = 'curl  -k \'' + getUrl + '\'';
+        console.log( curl );
+    }
+
+    let headers = new HttpHeaders( {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    } );
+
+    let options = {
+        headers: headers,
+        method: 'get',
+    };
+
+    let results;
+    try{
+        results = this.httpClient.get( getUrl, options ).pipe( timeout( Properties.HTTP_TIMEOUT ) );
+    }catch( e ){
+        // TODO react to error.
+        console.error( 'doGet Exception: ' + e );
+    }
+    return results;
+
+} 
 
 }
