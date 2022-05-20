9227a7216c46dede3a68d0e01d110938f79f450a
regulator95 <einolfs@nih.gov>
2022-05-20 14:20:39 -0400

    finished curation

--- software/cananolab-client-new/src/app/cananolab-client/main-display/curation/curation.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/curation/curation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Properties } from '../../../../assets/properties';
+import { Consts } from '../../../constants';
 
 @Component({
   selector: 'canano-curation',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curation.component.scss']
 })
 export class CurationComponent implements OnInit {
+  data;
+  helpUrl='https://wiki.nci.nih.gov/display/caNanoLab/Managing+Data+Curation#ManagingDataCuration-ReviewBatchResults';
+  toolHeadingNameManage = 'Review By Curator';
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+      let url=this.httpClient.get(Properties.API_SERVER_URL+'/caNanoLab/rest/curation/reviewData');
+      url.subscribe(data=> {
+        this.data=data;
+      },
+      error=> {
+          console.log('error')
+      })
   }
+
+
 
 }
