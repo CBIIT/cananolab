f2dc0fd44825afb5f588c7ecfdc027fe2bf675b7
regulator95 <einolfs@nih.gov>
2022-05-29 10:37:24 -0400

    adding rowOdd to curation table, adding There is no data pending review. when there are no records eliminating table

--- software/cananolab-client-new/src/app/cananolab-client/main-display/curation/curation.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/curation/curation.component.ts
@@ -13,30 +13,12 @@ export class CurationComponent implements OnInit {
     data;
     helpUrl='https://wiki.nci.nih.gov/display/caNanoLab/Managing+Data+Curation#ManagingDataCuration-ReviewBatchResults';
     toolHeadingNameManage = 'Review By Curator';
-    tempCuration=[
-        {
-           "dataId":"53346305",
-           "dataName":"UChicago-KLuJACS2015-01",
-           "dataType":"sample",
-           "submittedDate":"2015-11-22",
-           "submittedBy":"chunbaih",
-           "reviewStatus":"pending",
-           "reviewLink":"/caNanoLab/#/editSample?sampleId=53346305"},
-           {
-            "dataId":"53346305",
-            "dataName":"UChicago-KLuJACS2015-01",
-            "dataType":"sample",
-            "submittedDate":"2015-11-22",
-            "submittedBy":"chunbaih",
-            "reviewStatus":"pending",
-            "reviewLink":"/caNanoLab/#/editSample?sampleId=53346305"}        ]
     constructor(private httpClient:HttpClient) { }
 
     ngOnInit(): void {
         let url=this.httpClient.get(Properties.API_SERVER_URL+'/caNanoLab/rest/curation/reviewData');
         url.subscribe(data=> {
             this.data=data;
-            this.data=this.tempCuration
         },
         error=> {
             console.log('error')
