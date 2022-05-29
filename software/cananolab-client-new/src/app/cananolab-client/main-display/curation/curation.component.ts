e86ed6133118b726be3759a3e76c263e660ecad2
regulator95 <einolfs@nih.gov>
2022-05-29 10:50:06 -0400

    added correct link for publication edit on curation page

--- software/cananolab-client-new/src/app/cananolab-client/main-display/curation/curation.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/curation/curation.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Properties } from '../../../../assets/properties';
 import { Consts } from '../../../constants';
 import { ConsoleReporter } from 'jasmine';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'canano-curation',
@@ -11,18 +12,25 @@ import { ConsoleReporter } from 'jasmine';
 })
 export class CurationComponent implements OnInit {
     data;
+    errors={};
     helpUrl='https://wiki.nci.nih.gov/display/caNanoLab/Managing+Data+Curation#ManagingDataCuration-ReviewBatchResults';
     toolHeadingNameManage = 'Review By Curator';
-    constructor(private httpClient:HttpClient) { }
+    constructor(private router:Router,private httpClient:HttpClient) { }
 
     ngOnInit(): void {
         let url=this.httpClient.get(Properties.API_SERVER_URL+'/caNanoLab/rest/curation/reviewData');
         url.subscribe(data=> {
             this.data=data;
+            this.errors={};
         },
         error=> {
+            this.errors=error;
             console.log('error')
         })
+    }
+
+    editPublication(record) {
+        this.router.navigate(['/home/samples/publications/publication',record.dataId]);
     }
 
 
