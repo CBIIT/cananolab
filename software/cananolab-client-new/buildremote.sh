9d148bc6a1c765e07a1e8a8a6c5a11a02741a54f
regulator95 <einolfs@nih.gov>
2022-06-01 12:40:32 -0400

    fixed a few things in buildremote

--- software/cananolab-client-new/buildremote.sh
+++ software/cananolab-client-new/buildremote.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-rm -rf cananolab_new_client
-ng build --base-href '.' --output-path cananolab_new_client
-cd cananolab_new_client
+npm install
+rm -rf /local/home/wildfly8a/cananolab_new_client
+ng build --base-href '.' --output-path /local/home/wildfly8a/cananolab_new_client
+cd /local/home/wildfly8a/cananolab_new_client
 jar -cvf cananolab_new_client.war *
