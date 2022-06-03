4ada62ef59ada204c0bd64f6e0c793aab461aa3e
lernermh <martin.lerner@nih.gov>
2022-06-03 09:30:11 -0400

    Fix QUERY_SAMPLE_AVAILABILITY path

--- software/cananolab-client-new/src/app/constants.ts
+++ software/cananolab-client-new/src/app/constants.ts
@@ -22,7 +22,7 @@ export const Consts = {
     QUERY_SAMPLE_ADVANCED_SEARCH_SETUP: 'caNanoLab/rest/sample/setupAdvancedSearch',
     QUERY_SAMPLE_ADVANCED_SEARCH: 'caNanoLab/rest/sample/searchSampleAdvanced',
     QUERY_SEARCH_SAMPLE: 'caNanoLab/rest/sample/searchSample', // @TODO rename this SAMPLE_SEARCH
-    QUERY_SAMPLE_AVAILABILITY: 'caNanoLab/rest/sample/viewDataAvailability',
+    QUERY_SAMPLE_AVAILABILITY: 'caNanoLab/rest/sample/regenerateDataAvailability',
     QUERY_SAMPLE_UPDATE: 'caNanoLab/rest/sample/updateSample',
     QUERY_SAMPLE_AVAILABILITY_HTML: 'caNanoLab/views/sample/view/sampleDataAvailability.html',  // @FIXME That's not how this works
     QUERY_SAMPLE_POC_UPDATE_SAVE:  'caNanoLab/rest/sample/savePOC',
