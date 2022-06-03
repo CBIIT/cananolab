f23230fb860b8d701ab613ac8b9a95c2dcc20f3f
regulator95 <einolfs@nih.gov>
2022-06-02 23:43:01 -0400

    added in sorting for publication, sample, protocol and main searches

--- software/cananolab-client-new/src/app/cananolab-client/main-display/protocols/protocol-search/protocol-search-results/protocol-search-results.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/protocols/protocol-search/protocol-search-results/protocol-search-results.component.ts
@@ -11,14 +11,22 @@ import { ProtocolsService } from '../../protocols.service';
 import { ApiService } from '../../../../common/services/api.service';
 import { StatusDisplayService } from '../../../../status-display/status-display.service';
 import { Router } from '@angular/router';
-@Component( {
+@Component({
     selector: 'canano-protocol-search-results',
     templateUrl: './protocol-search-results.component.html',
-    styleUrls: ['./protocol-search-results.component.scss']
-} )
-export class ProtocolSearchResultsComponent implements OnInit, OnDestroy{
+    styleUrls: ['./protocol-search-results.component.scss'],
+})
+export class ProtocolSearchResultsComponent implements OnInit, OnDestroy {
     searchResults;
-    columnHeadings = ['Actions', 'Type', 'Name', 'Abbreviation', 'Version', 'File', 'Created'];
+    columnHeadings = [
+        {Actions:'Actions'},
+        {type:'Protocol Type'},
+        {viewName:'Protocol Name'},
+        {abbreviation:'Abbreviation'},
+        {version:'Version'},
+        {fileInfo:'File'},
+        {createdDate:'Created'}
+    ];
     maxPageLength = Properties.MAX_PAGE_LENGTH;
     pageLength = Properties.DEFAULT_PAGE_LENGTH;
     pageCount = 10;
@@ -26,7 +34,15 @@ export class ProtocolSearchResultsComponent implements OnInit, OnDestroy{
     currentPage = 0;
     searchResultsPageToDisplay;
 
-    sortingStates = [SortState.NO_SORT, SortState.SORT_UP, SortState.NO_SORT, SortState.NO_SORT, SortState.NO_SORT, SortState.NO_SORT, SortState.NO_SORT];
+    sortingStates = [
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT
+    ];
 
     sortState = SortState;
     properties = Properties;
@@ -38,161 +54,100 @@ export class ProtocolSearchResultsComponent implements OnInit, OnDestroy{
 
     private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
 
-    constructor( private router:Router,private searchResultsPagerService: SearchResultsPagerService, private utilService: UtilService,
-                 private protocolsService: ProtocolsService, private apiService: ApiService, private statusDisplayService: StatusDisplayService ){
-    }
+    constructor(
+        private router: Router,
+        private searchResultsPagerService: SearchResultsPagerService,
+        private utilService: UtilService,
+        private protocolsService: ProtocolsService,
+        private apiService: ApiService,
+        private statusDisplayService: StatusDisplayService
+    ) {}
 
-    ngOnInit(){
-        this.searchResults=this.protocolsService.getProtocolSearchResults();
-        this.searchResultsPagerService.currentPageChangeEmitter.pipe( takeUntil( this.ngUnsubscribe ) )
-            .subscribe( ( data ) => {
+    ngOnInit() {
+        this.searchResults = this.protocolsService.getProtocolSearchResults();
+        this.searchResultsPagerService.currentPageChangeEmitter
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe((data) => {
                 this.currentPage = data;
                 this.setupPage();
-            } );
+            });
 
-
-
-
-        this.statusDisplayService.updateUserEmitter.pipe( timeout( Properties.HTTP_TIMEOUT ) ).subscribe(
-            data => {
+        this.statusDisplayService.updateUserEmitter
+            .pipe(timeout(Properties.HTTP_TIMEOUT))
+            .subscribe((data) => {
                 this.userName = data;
             });
 
-
-
         this.searchResultsCount = this.searchResults.length;
-        this.pageCount = Math.ceil( this.searchResultsCount / this.pageLength );
+        this.pageCount = Math.ceil(this.searchResultsCount / this.pageLength);
         this.onPageLengthChange();
     }
 
-    onPageLengthChange(){
-        if( this.pageLength < 1 ){
+    onPageLengthChange() {
+        if (this.pageLength < 1) {
             this.pageLength = 1;
         }
-        if( this.pageLength > this.maxPageLength ){
+        if (this.pageLength > this.maxPageLength) {
             this.pageLength = this.maxPageLength;
         }
 
-        this.pageCount = Math.ceil( this.searchResultsCount / this.pageLength );
-        this.searchResultsPagerService.setPageCount( this.pageCount );
+        this.pageCount = Math.ceil(this.searchResultsCount / this.pageLength);
+        this.searchResultsPagerService.setPageCount(this.pageCount);
         this.setupPage(); // Sets this page as the currently viewed search results.
     }
 
-    setupPage(){
-        this.searchResultsPageToDisplay = this.searchResults.slice( this.pageLength * this.currentPage, this.pageLength * (this.currentPage + 1) );
-
+    setupPage() {
+        this.searchResultsPageToDisplay = this.searchResults.slice(
+            this.pageLength * this.currentPage,
+            this.pageLength * (this.currentPage + 1)
+        );
     }
 
-    parseFileData( fileData: string ){
+    parseFileData(fileData: string) {}
 
-
+    onSortClick(i, key) {
+        if (i) {
+            if (this.sortingStates[i]) {
+                // clicking on column that already is sorted on //
+                this.sortingStates[i] = this.sortingStates[i] == 1 ? 2 : 1;
+            } else {
+                // reset sorting states //
+                this.sortingStates.forEach((item, index) => {
+                    this.sortingStates[index] = 0;
+                });
+                this.sortingStates[i] = 1;
+            }
+            if (this.sortingStates[i] == 1) {
+                this.searchResults.sort((a, b) => (a[key] < b[key] ? 1 : -1));
+            } else {
+                this.searchResults.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+            }
+            this.setupPage();
+        }
     }
 
-    onSortClick( c ){
-        // No sorting for "Actions" column
-        if( c < 1 ){
-            return;
-        }
-
-        let temp = -1;
-
-        // Set temp to new setting
-        switch( this.sortingStates[c] ){
-            case SortState.NO_SORT:
-                temp = SortState.SORT_UP;
-                break;
-            case SortState.SORT_UP:
-                temp = SortState.SORT_DOWN;
-                break;
-            case SortState.SORT_DOWN:
-                temp = SortState.SORT_UP;
-                break;
-        }
-        // END Set temp to new setting
-
-        // Clear what ever may have been set
-        for( let n = 0; n < this.sortingStates.length; n++ ){
-            this.sortingStates[n] = SortState.NO_SORT;
-        }
-
-        // Set the ne sort state
-        this.sortingStates[c] = temp;
-
-        this.doSort();
-    }
-
-    onEditClick( protocolToEdit ){
-        this.router.navigate(['home/protocols/edit-protocol',protocolToEdit.id])
+    onEditClick(protocolToEdit) {
+        this.router.navigate([
+            'home/protocols/edit-protocol',
+            protocolToEdit.id,
+        ]);
         // this.protocolsService.setCurrentProtocolScreen( ProtocolScreen.PROTOCOL_EDIT_SCREEN, protocolToEdit.id );
     }
 
-    onViewClick( protocolToView ){
-        this.protocolsService.setCurrentProtocolScreen( ProtocolScreen.PROTOCOL_VIEW_SCREEN, protocolToView.id );
+    onViewClick(protocolToView) {
+        this.protocolsService.setCurrentProtocolScreen(
+            ProtocolScreen.PROTOCOL_VIEW_SCREEN,
+            protocolToView.id
+        );
     }
 
-    doSort(){
-        let column = -1;
-        let dir = -1;
-        for( let n = 0; n < this.sortingStates.length; n++ ){
-            if( this.sortingStates[n] !== SortState.NO_SORT ){
-                dir = this.sortingStates[n];
-                column = n;
-            }
-
-            // Type
-            switch( column ){
-                case 1:
-                    this.searchResults.sort( ( row1, row2 ) =>
-                        row1.type.localeCompare( row2.type ) *
-                        (dir === SortState.SORT_DOWN ? -1 : 1)
-                    );
-                    break;
-
-                // viewName "Name"
-                case 2:
-                    this.searchResults.sort( ( row1, row2 ) =>
-                        row1.viewName.localeCompare( row2.viewName ) *
-                        (dir === SortState.SORT_DOWN ? -1 : 1)
-                    );
-                    break;
-
-                // abbreviation
-                case 3:
-                    this.searchResults.sort( ( row1, row2 ) =>
-                        row1.abbreviation.localeCompare( row2.abbreviation ) *
-                        (dir === SortState.SORT_DOWN ? -1 : 1)
-                    );
-                    break;
-
-                // Version
-                case 4:
-                    this.searchResults.sort( ( n1, n2 ) => (n1.version - n2.version) *
-                        (dir === SortState.SORT_DOWN ? -1 : 1)
-                    );
-                    break;
-
-                // fileInfo
-                case 5:
-                    this.searchResults.sort( ( row1, row2 ) =>
-                        row1.fileInfo.localeCompare( row2.fileInfo ) *
-                        (dir === SortState.SORT_DOWN ? -1 : 1)
-                    );
-                    break;
-
-                // createdDate
-                case 6:
-                    this.searchResults.sort( ( row1, row2 ) => row2.createdDate - row1.createdDate *
-                        (dir === SortState.SORT_DOWN ? -1 : 1)
-                    );
-                    break;
-            }
-
-        }
-        this.setupPage();
-    }
-
-    addToFavorites( protocolId, protocolName, fileId, editable, protocolFileTitle ){
-
+    addToFavorites(
+        protocolId,
+        protocolName,
+        fileId,
+        editable,
+        protocolFileTitle
+    ) {
         // api.doPost
         let queryData = {
             dataType: 'protocol',
@@ -201,60 +156,22 @@ export class ProtocolSearchResultsComponent implements OnInit, OnDestroy{
             protocolFileId: fileId,
             protocolFileTitle: protocolFileTitle,
             editable: true,
-            loginName: 'canano_curator'
+            loginName: 'canano_curator',
         };
 
-        this.apiService.doPost( Consts.QUERY_ADD_FAVORITE, <any>queryData ).subscribe(
-            data => {
-                console.log( 'Data back from addToFavorites: ', data );
-            },
-            err => {
-                console.error( 'ERR Data back from addToFavorites: ', err );
-            }
-        );
+        this.apiService
+            .doPost(Consts.QUERY_ADD_FAVORITE, <any>queryData)
+            .subscribe(
+                (data) => {
+                    console.log('Data back from addToFavorites: ', data);
+                },
+                (err) => {
+                    console.error('ERR Data back from addToFavorites: ', err);
+                }
+            );
     }
 
-    /*
-
-        $scope.addToFavorites = function (protocolId, protocolName, fileId, editable, protocolFileTitle) {
-            $scope.loader = true;
-
-            $scope.favoriteBean = {
-                "dataType": "protocol",
-                "dataName": protocolName,
-                "dataId": protocolId,
-                "protocolFileId": fileId,
-                "editable": editable,
-                "protocolFileTitle": protocolFileTitle,
-                "loginName": $scope.loggedInUser.name
-            };
-
-            $http({
-                method: 'POST',
-                url: '/caNanoLab/rest/core/addFavorite',
-                data: $scope.favoriteBean
-            }).
-            then(function (data, status, headers, config) {
-                data = data['data']
-                var hrefEl = document.getElementById('href' + protocolId);
-                var msgEl = document.getElementById('msg' + protocolId);
-                msgEl.innerHTML = data;
-                hrefEl.style.visibility = "hidden";
-                $scope.loader = false;
-
-            }).
-            catch(function (data, status, headers, config) {
-                data = data['data']
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                // $rootScope.sampleData = data;
-                $scope.loader = false;
-                $scope.messages = data;
-            });
-        };
-     */
-
-    ngOnDestroy(){
+    ngOnDestroy() {
         this.ngUnsubscribe.next();
         this.ngUnsubscribe.complete();
     }
--- software/cananolab-client-new/src/app/cananolab-client/main-display/search-results/search-results/search-results.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/search-results/search-results/search-results.component.ts
@@ -1,133 +1,176 @@
 import { Component, OnInit } from '@angular/core';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import { TopKeywordSearchService } from '../../../header/top-keyword-search/top-keyword-search.service';
 import { Properties } from '../../../../../assets/properties';
 import { Consts } from '../../../../constants';
 import { SortState } from '../../../../constants';
 import { Subject } from 'rxjs';
 import { SearchResultsPagerService } from '../../../common/components/search-results-pager/search-results-pager.service';
-import { takeUntil,timeout } from 'rxjs/operators';
+import { takeUntil, timeout } from 'rxjs/operators';
 import { StatusDisplayService } from '../../../status-display/status-display.service';
 import { Router } from '@angular/router';
 import { ApiService } from '../../../common/services/api.service';
 @Component({
-  selector: 'canano-search-results',
-  templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+    selector: 'canano-search-results',
+    templateUrl: './search-results.component.html',
+    styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
-    searchResults;
-    maxPageLength = Properties.MAX_PAGE_LENGTH;
-    pageLength = Properties.DEFAULT_PAGE_LENGTH;
-    columnHeadings = ['Actions', 'Type', 'Name', 'Created Date', 'Description'];
-    errors={};
-    helpUrl = Consts.HELP_URL_SAMPLE_SEARCH;
-    toolHeadingNameSearchSample = 'Sample Search Results';
-    pageCount = 10;
-    searchResultsCount = -1;
+    columnHeadings = [
+        { actions: 'Actions' },
+        { type: 'Type' },
+        { name: 'Name' },
+        { createdDate: 'Created Date' },
+        { description: 'Description' },
+    ];
     currentPage = 0;
-    searchResultsPageToDisplay;
-
-    sortingStates = [SortState.NO_SORT, SortState.SORT_UP, SortState.NO_SORT, SortState.NO_SORT, SortState.NO_SORT, SortState.NO_SORT, SortState.NO_SORT];
-
-    sortState = SortState;
+    errors = {};
+    helpUrl = Consts.HELP_URL_SAMPLE_SEARCH;
+    maxPageLength = Properties.MAX_PAGE_LENGTH;
+    pageCount = 10;
+    pageLength = Properties.DEFAULT_PAGE_LENGTH;
     properties = Properties;
+    searchResults;
+    searchResultsCount = -1;
+    searchResultsPageToDisplay;
+    sortingStates = [
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+        SortState.NO_SORT,
+    ];
+    sortState = SortState;
+    toolHeadingNameSearchSample = 'Sample Search Results';
     userName;
 
     private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
-    constructor(private apiService:ApiService,private router:Router,private statusDisplayService:StatusDisplayService,
-                private searchResultsPagerService:SearchResultsPagerService,
-                private topKeywordSearchService:TopKeywordSearchService) { }
+    constructor(
+        private apiService: ApiService,
+        private router: Router,
+        private statusDisplayService: StatusDisplayService,
+        private searchResultsPagerService: SearchResultsPagerService,
+        private topKeywordSearchService: TopKeywordSearchService
+    ) {}
 
     ngOnInit(): void {
-        this.searchResults=this.topKeywordSearchService.getKeywordResults();
+        this.searchResults = this.topKeywordSearchService.getKeywordResults();
         this.searchResultsCount = this.searchResults.length;
-
-        this.searchResultsPagerService.currentPageChangeEmitter.pipe( takeUntil( this.ngUnsubscribe ) )
-            .subscribe( ( data ) => {
-                this.currentPage = data;
-                this.setupPage();
-                this.errors={};
-            },
-            errors=> {
-                this.errors=errors;
-            } );
-
-            console.log(this.properties)
-        this.statusDisplayService.updateUserEmitter.pipe( timeout( Properties.HTTP_TIMEOUT ) ).subscribe(
-            data => {
-                this.userName = data;
-                this.errors={};
-            },
-            error=>{
-                this.errors=error;
-                console.log('error')
-            } );
-
-
+        this.searchResultsPagerService.currentPageChangeEmitter
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(
+                (data) => {
+                    this.currentPage = data;
+                    this.setupPage();
+                    this.errors = {};
+                },
+                (errors) => {
+                    this.errors = errors;
+                }
+            );
+        this.statusDisplayService.updateUserEmitter
+            .pipe(timeout(Properties.HTTP_TIMEOUT))
+            .subscribe(
+                (data) => {
+                    this.userName = data;
+                    this.errors = {};
+                },
+                (error) => {
+                    this.errors = error;
+                    console.log('error');
+                }
+            );
         this.searchResultsCount = this.searchResults.length;
-        this.pageCount = Math.ceil( this.searchResultsCount / this.pageLength );
+        this.pageCount = Math.ceil(this.searchResultsCount / this.pageLength);
         this.onPageLengthChange();
-    }
-
-    navigateToResult( resultId,type ){
-        console.log(type)
-        if (type=='sample') {
-            this.router.navigate(['/home/samples/sample',resultId]);
-
-        }
-        if (type=='publication'){
-            this.router.navigate(['/home/samples/publications/publication',resultId]);
-        }
-        if (type=='protocol') {
-            this.router.navigate(['/home/protocols/edit-protocol',resultId]);
-
-
-        }
     }
 
     addToFavorites(result) {
         let data = {
-            "dataId":result.id,
-            "dataName":result.name,
-            "dataType":result.type,
-            "editable":result.editable,
-            "loginName":Properties.current_user
-        }
+            dataId: result.id,
+            dataName: result.name,
+            dataType: result.type,
+            editable: result.editable,
+            loginName: Properties.current_user,
+        };
 
-        if (result.type=='protocol') {
-            data['protocolFileId']=result.fileId;
-            data['protocolFileTitle']=result.fileTitle;
+        if (result.type == 'protocol') {
+            data['protocolFileId'] = result.fileId;
+            data['protocolFileTitle'] = result.fileTitle;
         }
-        this.apiService.doPost(Consts.QUERY_ADD_FAVORITE,data).subscribe(data=> {
-            this.errors={};
-        },
-        error=> {
-            this.errors=error;
-        })
+        this.apiService.doPost(Consts.QUERY_ADD_FAVORITE, data).subscribe(
+            (data) => {
+                this.errors = {};
+            },
+            (error) => {
+                this.errors = error;
+            }
+        );
     }
 
-    onPageLengthChange(){
-        if( this.pageLength < 1 ){
+    navigateToResultEdit(result) {
+        if (result.type == 'sample') {
+            this.router.navigate(['/home/samples/sample', result.id]);
+        }
+        if (result.type == 'publication') {
+            this.router.navigate([
+                '/home/samples/publications/publication',
+                result.id,
+            ]);
+        }
+        if (result.type == 'protocol') {
+            this.router.navigate(['/home/protocols/edit-protocol', result.id]);
+        }
+    }
+
+    navigateToResultView(result) {
+        if (result.type == 'sample') {
+            this.router.navigate(['/home/samples/view-sample', result.id]);
+        }
+        if (result.type == 'publication') {
+            console.log(result)
+        }
+        if (result.type == 'protocol') {
+            window.open(Consts.QUERY_DOWNLOAD_FILE+'?fileId='+result.fileId)
+        }
+    }
+
+    onPageLengthChange() {
+        if (this.pageLength < 1) {
             this.pageLength = 1;
         }
-        if( this.pageLength > this.maxPageLength ){
+        if (this.pageLength > this.maxPageLength) {
             this.pageLength = this.maxPageLength;
         }
-
-        this.pageCount = Math.ceil( this.searchResultsCount / this.pageLength );
-        this.searchResultsPagerService.setPageCount( this.pageCount );
-        this.setupPage(); // Sets this page as the currently vied search results.
+        this.pageCount = Math.ceil(this.searchResultsCount / this.pageLength);
+        this.searchResultsPagerService.setPageCount(this.pageCount);
+        this.setupPage();
     }
 
-    onSortClick( i ){
-        console.log( 'onSortClick: ', i );
+    onSortClick(i, key) {
+        if (i) {
+            if (this.sortingStates[i]) {
+                // clicking on column that already is sorted on //
+                this.sortingStates[i] = this.sortingStates[i] == 1 ? 2 : 1;
+            } else {
+                // reset sorting states //
+                this.sortingStates.forEach((item, index) => {
+                    this.sortingStates[index] = 0;
+                });
+                this.sortingStates[i] = 1;
+            }
+            if (this.sortingStates[i] == 1) {
+                this.searchResults.sort((a, b) => (a[key] < b[key] ? 1 : -1));
+            } else {
+                this.searchResults.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+            }
+            this.setupPage();
+        }
     }
 
-    setupPage(){
-        this.searchResultsPageToDisplay = this.searchResults.slice( this.pageLength * this.currentPage, this.pageLength * (this.currentPage + 1) );
-
+    setupPage() {
+        this.searchResultsPageToDisplay = this.searchResults.slice(
+            this.pageLength * this.currentPage,
+            this.pageLength * (this.currentPage + 1)
+        );
     }
-
-
 }
