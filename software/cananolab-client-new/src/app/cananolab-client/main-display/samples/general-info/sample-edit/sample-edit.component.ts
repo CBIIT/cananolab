6b53425e8758d0d478903767b1335280e8808579
Shaun Einolf <seinolf@gmail.com>
2022-06-07 16:55:57 -0400

    Update sample-edit.component.ts

--- software/cananolab-client-new/src/app/cananolab-client/main-display/samples/general-info/sample-edit/sample-edit.component.ts
+++ software/cananolab-client-new/src/app/cananolab-client/main-display/samples/general-info/sample-edit/sample-edit.component.ts
@@ -16,9 +16,18 @@ import { SampleAvailabilityDisplayService } from '../sample-search/sample-search
 export class SampleEditComponent implements OnInit, OnDestroy{
     helpUrl = Consts.HELP_URL_SAMPLE_EDIT;
     toolHeadingNameSearchSample = 'Update Sample';
+    availabilityEditIndex;
+    currentDropdownValues;
     sampleId = -1;
-    sampleData;
+    metricsDeleted;
+    data;
+    dataTrailer;
+    dataAvailability;
+    errors;
+    message;
     myParam;
+    pointOfContactIndex;
+    pointOfContact;
     newKeyword = '';
     pointOfContacts;
     showPointOfContactCreate = false;
@@ -30,6 +39,8 @@ export class SampleEditComponent implements OnInit, OnDestroy{
 
     ngOnInit(): void{
         this.navigationService.setCurrentSelectedItem(0);
+        this.currentDropdownValues = {};
+        this.errors={};
         this.route.params.subscribe(
             ( params: Params ) => {
                 this.sampleId = params['sampleId'].replace( /^.*\?sampleId=/, '' );
@@ -40,20 +51,109 @@ export class SampleEditComponent implements OnInit, OnDestroy{
                     Properties.CURRENT_SAMPLE_ID = this.sampleId;
                 }
 
-                this.sampleData = this.getSampleEditData().subscribe(
+                this.getSampleEditData().subscribe(
                     data => {
                         Properties.SAMPLE_TOOLS = true;
-                        this.sampleData = data;
+                        this.data = data;
+                        console.log(data)
+                        this.data.keywords=this.joinKeywords(this.data.keywords);
+                        this.dataTrailer=JSON.parse(JSON.stringify(this.data))
                         Properties.CURRENT_SAMPLE_NAME = data['sampleName'];
-                        this.pointOfContacts = this.sampleData.pointOfContacts;
+                        this.pointOfContacts = this.data.pointOfContacts;
                     } );
             } );
     }
 
+    // set pointer fields to old values when adding other //
+    addOtherValue(field,currentValue) {
+        this.currentDropdownValues[field]=currentValue;
+    };
+
+    // save other value //
+    saveOther(newItem: Object) {
+        if (newItem['change'] && newItem['value']) {
+            this.addDropdownItem(newItem['array'],newItem['value'])
+            this.setValue(newItem['field'],newItem['value']);
+        }
+        else {
+            this.setValue(newItem['field'],newItem['value']);
+        }
+    };
+
+    deleteKeyword(index) {
+        console.log(index)
+    }
+
+    addPointOfContact() {
+        this.pointOfContact={organization:{name:""},address:{},role:""};
+        this.pointOfContactIndex=-1;
+        setTimeout(function () {
+            document.getElementById('pocForm').scrollIntoView();
+        }, 100);
+    }
+
+    cancelPointOfContact() {
+        this.pointOfContactIndex=null;
+    }
+
+    deletePointOfContact() {
+        if (confirm("Are you sure you wish to delete this point of contact?")) {
+            this.pointOfContact['sampleId']=this.sampleId;
+            this.apiService.doPost(Consts.QUERY_SAMPLE_POC_UPDATE_DELETE,this.pointOfContact).subscribe(data=> {
+                this.errors={};
+                this.data=data;
+                this.data.keywords=this.joinKeywords(this.data.keywords);
+                this.dataTrailer=JSON.stringify(JSON.parse(this.data));
+                this.pointOfContactIndex=null;
+            },
+            errors=> {
+                this.errors=errors;
+            })
+        }
+    }
+
+    editPointOfContact(index,poc) {
+        this.pointOfContact = JSON.parse(JSON.stringify(poc));
+        this.pointOfContactIndex=index;
+        setTimeout(function () {
+            document.getElementById('pocForm').scrollIntoView();
+        }, 100);
+    }
+
+    savePointOfContact() {
+        this.pointOfContact['dirty']=true;
+        if (this.pointOfContactIndex==-1) {
+            this.data.pointOfContacts.push(this.pointOfContact)
+            console.log(this.data)
+        }
+        else {
+            this.data['pointOfContacts'][this.pointOfContactIndex]=this.pointOfContact;
+        }
+        this.data['keywords'] = this.data['keywords'].split('\n');
+
+        this.apiService.doPost(Consts.QUERY_SAMPLE_POC_UPDATE_SAVE,this.data).subscribe(data=> {
+            data['keywords']=this.joinKeywords(this.data['keywords'])
+            this.data=data;
+            this.dataTrailer=JSON.parse(JSON.stringify(data));
+            this.pointOfContactIndex=null;
+        })
+    }
+
+    joinKeywords(keywords) {
+        if (keywords) {
+            return keywords.join('\n')
+
+        }
+        console.log(keywords)
+    }
+
+
+
     onAddKeyword( newKeyword ){
-        this.sampleData['keywords'].push( newKeyword );
+        this.data['keywords'].push( newKeyword );
         this.newKeyword = '';
     }
+
 
     onSampleDeleteClick(){
         console.log( 'onSampleDeleteClick' );
@@ -64,25 +164,29 @@ export class SampleEditComponent implements OnInit, OnDestroy{
     }
 
     onSampleResetClick(){
-        console.log( 'onSampleResetClick' );
+       this.data=JSON.parse(JSON.stringify(this.dataTrailer));
+       this.data.keywords=this.joinKeywords(this.data.keywords);
     }
 
     onSampleUpdateClick(){
-        console.log( 'onSampleUpdateClick sampleName: ', this.sampleData['sampleName'] );
-        console.log( 'onSampleUpdateClick sampleId: ', this.sampleData['sampleId'] );
-        console.log( 'onSampleUpdateClick keywords: ', this.sampleData['keywords'] );
+        console.log( 'onSampleUpdateClick sampleName: ', this.data['sampleName'] );
+        console.log( 'onSampleUpdateClick sampleId: ', this.data['sampleId'] );
+        console.log( 'onSampleUpdateClick keywords: ', this.data['keywords'] );
         this.updateSample();
     }
 
     updateSample(){
         let su = {};
-        su['sampleName'] = this.sampleData['sampleName'];
-        su['sampleId'] = this.sampleData['sampleId'];
-        su['keywords'] = this.sampleData['keywords'];
+        su['sampleName'] = this.data['sampleName'];
+        su['sampleId'] = this.data['sampleId'];
+        su['keywords'] = this.data['keywords'].split('\n');
 
         this.apiService.doPost( Consts.QUERY_SAMPLE_UPDATE, su ).subscribe(
             data => {
-                console.log( 'Return data from QUERY_SAMPLE_UPDATE: ', data );
+                this.data=data;
+                this.data.keywords=this.joinKeywords(this.data.keywords);
+                this.dataTrailer=JSON.parse(JSON.stringify(JSON.parse(this.data)));
+                this.message='Sample Updated'
             },
             ( err ) => {
                 console.log( 'ERROR QUERY_SAMPLE_UPDATE: ', err );
@@ -118,20 +222,59 @@ export class SampleEditComponent implements OnInit, OnDestroy{
 
     }
 
+    hideDataAvailability(event) {
+        let target = event.target.id;
+        console.log(target)
+        // if (target!='availabilityMetrics' && target!='editDataAvailabilityButton') {
+        //     this.availabilityEditIndex=null;
+        // }
+    }
+
     onAddPocClick(){
         this.pointOfContactService.showPointOfContactCreateEmitter.emit( true );
         this.showPointOfContactCreate = true;
     }
 
 
-    onAvailabilityClick( id ){
-        this.apiService.doGet( Consts.QUERY_SAMPLE_AVAILABILITY, 'sampleId=' + id).subscribe(
+    onAvailabilityClick( ){
+        this.apiService.doGet( Consts.QUERY_SAMPLE_AVAILABILITY, 'sampleId=' + this.sampleId).subscribe(
             data => {
                 this.sampleAvailabilityDisplayService.displayStuff( data );
+                this.data=data;
+                this.data['keywords']=this.joinKeywords(this.data['keywords']);
+                this.dataTrailer=JSON.parse(JSON.stringify(this.data));
             },
             ( err ) => {
                 console.log( 'ERROR QUERY_SAMPLE_AVAILABILITY: ', err );
             } );
+    }
+
+
+    onAvailabilityDeleteClick( ){
+        if (confirm("Are you sure you wish to delete the data availability metric?")) {
+            this.data['keywords'] = this.data['keywords'].split('\n');
+
+            this.apiService.doPost( Consts.QUERY_SAMPLE_DELETE_AVAILABILITY, this.data).subscribe(
+                data => {
+                    this.data=data;
+                    this.data['keywords']=this.joinKeywords(this.data['keywords']);
+                    this.dataTrailer=JSON.parse(JSON.stringify(this.data));
+                },
+                ( err ) => {
+                    console.log( 'ERROR QUERY_SAMPLE_AVAILABILITY: ', err );
+                } );
+        }
+
+    }
+
+    onAvailabilityEdit(event) {
+        this.availabilityEditIndex=1;
+        this.apiService.doGet(Consts.QUERY_SAMPLE_VIEW_AVAILABILITY,'sampleId='+this.sampleId).subscribe(data=> {
+            this.dataAvailability=data;
+        },
+        errors=> {
+            this.errors=errors;
+        })
     }
 
     ngOnDestroy(): void{
